00ef528053c81cb1257b18c6d517b3d13d3f326d
Arif Ullah <arifullah565360@gmail.com>
2021-05-05 08:18:35 +0600

    6th

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Navber from '../Navber/Navber';
 import Skills from '../Skills/Skills';
 
 
--- src/component/Navber/Navber.js
+++ src/component/Navber/Navber.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import { useState } from 'react';
-
 import { Link } from 'react-router-dom';
 import './Nabver.css'
-
 const Navber = () => {
     const [navber , setNavber] = useState(false);
 
